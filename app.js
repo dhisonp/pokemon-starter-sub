49774616bb40df846dbe03c21a2d4d0c7b2e491d
Dhison <dhisonp@gmail.com>
2021-11-16 18:09:29 -0500

    Cleaned the code a little bit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const reformat = (types) => {
 
   types.forEach((e) => {
     let id = e.name;
-    let val = [e.strengths];
+    let val = e.strengths;
     map.set(id, val);
   });
 
@@ -14,35 +14,45 @@ const reformat = (types) => {
 };
 
 const search = (types) => {
-  let set = new Set();;
+  let set = new Set();
   let flags = [];
   let count = 0;
   types.forEach((strength, type) => {
-    // console.log(`1: ${type}: ${strength}`);
     let a = type;
     let b, c;
-    strength[0].forEach((type_b) => {
+    strength.forEach((type_b) => {
       let strengths_b = types.get(type_b);
-      //   console.log("2: " + strengths_b);
-      strengths_b[0].forEach((type_c) => {
+      strengths_b.forEach((type_c) => {
         let strengths_c = types.get(type_c);
-        // console.log("3: " + strengths_c);
-        if (strengths_c[0].includes(a)) {
+        if (strengths_c.includes(a)) {
           let b = type_b;
           let c = type_c;
-        //   console.log(`Found: ${a} => ${b} => ${c}`);
+          // console.log(`Found: ${a} => ${b} => ${c}`);
           set.add([a, b, c]);
         } else {
         }
       });
     });
   });
-    console.log(set);
+  console.log("Found starter sub elements: ")
+  console.log(set);
 };
 
-//Procedure
+////Procedure
 let types = JSON.parse(fs.readFileSync("types.json"));
 
 types = reformat(types);
 search(types);
 // console.log(types);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+///// Notes ///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//Stackoverflow link: https://stackoverflow.com/questions/69978723/created-a-script-to-output-possible-pokemon-starter-elements-alternatives-fire
+//O(n^3) is the lowest complexity possible for the initial query.
+//O(1) is possible by writing the found results into a file and reading it afterwards.
+//Using the path data structure will simplify the code by a lot, but will retain the O(n^3) time complexity.
+//To remove the duplicates, you are to write a function after the intitial query is output.
+
